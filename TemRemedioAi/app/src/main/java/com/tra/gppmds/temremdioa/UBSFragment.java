d990e35d8d933a5a0b870d59588996c6742b98e7
carolinaferreira <carol.mf14@gmail.com>
2016-05-01 16:32:25 -0300

    Implementing UBSFragment.java

--- TemRemedioAi/app/src/main/java/com/tra/gppmds/temremdioa/UBSFragment.java
+++ TemRemedioAi/app/src/main/java/com/tra/gppmds/temremdioa/UBSFragment.java
@@ -1,7 +1,30 @@
 package com.tra.gppmds.temremdioa;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by carolina on 01/05/16.
  */
-public class UBSFragment {
+public class UBSFragment extends Fragment{
+    public UBSFragment(){
+    }
+
+    public static UBSFragment newInstance(){
+       return new UBSFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_ubs, container, false);
+        RecyclerView recyclerView = (RecyclerView) ;
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
