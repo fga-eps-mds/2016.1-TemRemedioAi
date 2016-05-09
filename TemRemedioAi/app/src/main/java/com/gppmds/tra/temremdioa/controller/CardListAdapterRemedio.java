f057a90ada15c7c18fec64be307db929e6ee124d
Guilherme <guilhermebaldissera@hotmail.com>
2016-05-09 20:32:04 -0300

    Adjust layout of List of Medicins

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/CardListAdapterRemedio.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/CardListAdapterRemedio.java
@@ -3,6 +3,7 @@ package com.gppmds.tra.temremdioa.controller;
 import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -10,7 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.gppmds.tra.temremdioa.model.Remedio;
@@ -31,20 +33,22 @@ public class CardListAdapterRemedio extends RecyclerView.Adapter<CardListAdapter
         public TextView textViewQuantidadePorcao;
         public TextView textViewNivelAtMedicamento;
         public CardView cardViewRemedio;
-        public LinearLayout headerLayout;
-        public LinearLayout expandLayout;
+        public RelativeLayout headerLayout;
+        public RelativeLayout expandLayout;
         public ValueAnimator mAnimator;
+        public Button buttonSelecionaUbs;
 
         public ViewHolder(CardView card) {
             super(card);
             this.textViewNomeMedicamento = (TextView) card.findViewById(R.id.textViewNomeMedicamento);
             this.textViewTipoMedicamento = (TextView) card.findViewById(R.id.textViewTipoMedicamento);
-            this.textViewQuantidadePorcao = (TextView) card.findViewById(R.id.textViewQuantidadePorcao);
+            this.textViewQuantidadePorcao = (TextView) card.findViewById(R.id.textViewQuantidadeMedicamento);
             this.textViewNivelAtMedicamento = (TextView) card.findViewById(R.id.textViewNivelAtMedicamento);
             this.cardViewRemedio = card;
-            this.expandLayout = (LinearLayout) card.findViewById(R.id.expandable);
+            this.expandLayout = (RelativeLayout) card.findViewById(R.id.expandable);
             this.expandLayout.setVisibility(View.GONE);
-            this.headerLayout = (LinearLayout) card.findViewById(R.id.header);
+            this.headerLayout = (RelativeLayout) card.findViewById(R.id.header);
+            this.buttonSelecionaUbs = (Button) card.findViewById(R.id.buttonSelecionarUbs);
 
             this.expandLayout.getViewTreeObserver().addOnPreDrawListener(
                     new ViewTreeObserver.OnPreDrawListener() {
@@ -66,13 +70,21 @@ public class CardListAdapterRemedio extends RecyclerView.Adapter<CardListAdapter
             this.headerLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (expandLayout.getVisibility()==View.GONE){
+                    if (expandLayout.getVisibility() == View.GONE) {
                         Log.i("log", "expand click");
                         expand();
-                    }else{
+                    } else {
                         Log.i("log", "collapse click");
                         collapse();
                     }
+                }
+            });
+
+            this.buttonSelecionaUbs.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(contextOpen, SelectUBSActivity.class);
+                    contextOpen.startActivity(intent);
                 }
             });
         }
@@ -143,7 +155,7 @@ public class CardListAdapterRemedio extends RecyclerView.Adapter<CardListAdapter
         Remedio rowData = this.dataRemedio.get(position);
         holder.textViewNomeMedicamento.setText(rowData.getMedDes());
         holder.textViewTipoMedicamento.setText(rowData.getUnidadeFormated());
-//      holder.textViewQuantidadePorcao.setText(rowData.getUnid());
+        holder.textViewQuantidadePorcao.setText("300 mg");
         holder.textViewNivelAtMedicamento.setText(rowData.getNivelAtencaoFormated());
     }
 
