a5045959b624195d9c020159528da23ffbf476a6
ElmarRoberto <elmarunb@gmail.com>
2016-05-09 17:14:43 -0300

    Implementation of Filterable in CardListAdapterRemedio and CardListAdapterUBS

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/adapter/CardListAdapterRemedio.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/adapter/CardListAdapterRemedio.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import com.gppmds.tra.temremdioa.controller.adapter.holder.ViewHolderRemedio;
 import com.gppmds.tra.temremdioa.model.Remedio;
@@ -15,9 +17,11 @@ import java.util.List;
 /**
  * Created by carolina on 04/05/16.
  */
-public class CardListAdapterRemedio extends RecyclerView.Adapter<ViewHolderRemedio>{
+public class CardListAdapterRemedio extends RecyclerView.Adapter<ViewHolderRemedio> implements Filterable{
     private List<Remedio> dataRemedio;
+    private List<Remedio> filterDataRemedio;
     private Context contextOpen;
+    //FilterSearchs filter;
 
     public CardListAdapterRemedio(Context context, List<Remedio> dataRemedio) {
         this.contextOpen = context;
@@ -38,6 +42,16 @@ public class CardListAdapterRemedio extends RecyclerView.Adapter<ViewHolderRemed
         holder.textViewTipoMedicamento.setText(rowData.getUnidadeFormated());
 //      holder.textViewQuantidadePorcao.setText(rowData.getUnid());
         holder.textViewNivelAtMedicamento.setText(rowData.getNivelAtencaoFormated());
+    }
+
+    @Override
+    public Filter getFilter() {
+//        if(filter == null) {
+//            filter = new CustomFilter( filterDataRemedio,this );
+//        }
+//
+//        return filter;
+        return null;
     }
 
     @Override
