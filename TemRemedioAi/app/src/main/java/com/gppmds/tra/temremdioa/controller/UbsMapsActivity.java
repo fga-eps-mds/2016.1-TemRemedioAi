a7375b05f7ca9581abe6dcff9c908fc0c1a4439c
João Pedro Pereira <jplpereira@gmail.com>
2016-05-26 18:19:01 -0300

    Resolving exception when Gmaps is not installed

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/UbsMapsActivity.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/UbsMapsActivity.java
@@ -1,9 +1,13 @@
 package com.gppmds.tra.temremdioa.controller;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -59,15 +63,65 @@ public class UbsMapsActivity extends AppCompatActivity implements OnMapReadyCall
             @Override
             public void onClick(View view) {
 
-                //Get latitude and longitude from ubs Holder and open with GMaps
-                String uri = "http://maps.google.com/maps?saddr="+"&daddr="+latitude+","+longitude;
-                Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
-                intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-                startActivity(intent);
+                setUpMapIfNeeded();
 
             }
         });
 
+    }
+
+    private void setUpMapIfNeeded() {
+
+
+        if(isGoogleMapsInstalled())
+        {
+            //Get latitude and longitude from ubs Holder and open with GMaps
+            String uri = "http://maps.google.com/maps?saddr="+"&daddr="+latitude+","+longitude;
+            Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+            intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+            startActivity(intent);
+
+        }
+        else
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Instalar Google Maps");
+            builder.setCancelable(true);
+            builder.setNegativeButton("Cancelar",null);
+            builder.setPositiveButton("Instalar", getGoogleMapsListener());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+
+    public boolean isGoogleMapsInstalled()
+    {
+        try
+        {
+            ApplicationInfo info = getPackageManager().getApplicationInfo("com.google.android.apps.maps", 0 );
+            return true;
+        }
+        catch(PackageManager.NameNotFoundException e)
+        {
+            return false;
+        }
+    }
+
+    public DialogInterface.OnClickListener getGoogleMapsListener()
+    {
+        return new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.google.android.apps.maps"));
+                startActivity(intent);
+
+                //Finish the activity so they can't circumvent the check
+                finish();
+            }
+        };
     }
 
     @Override
