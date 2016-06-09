2eceb45e06e5136172f1c2f651a9cb085f4922bb
Gabriel Climaco <climaco.cafe@gmail.com>
2016-06-09 20:06:53 -0300

    Adding two more unit tests

--- TemRemedioAi/app/src/test/java/com/gppmds/tra/temremdioa/TabsAdapterTest.java
+++ TemRemedioAi/app/src/test/java/com/gppmds/tra/temremdioa/TabsAdapterTest.java
@@ -4,6 +4,8 @@ import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 
 import com.gppmds.tra.temremdioa.controller.adapter.TabsAdapter;
+import com.gppmds.tra.temremdioa.controller.fragment.MedicineFragment;
+import com.gppmds.tra.temremdioa.controller.fragment.UBSFragment;
 import com.tra.gppmds.temremdioa.BuildConfig;
 
 import org.junit.Before;
@@ -12,7 +14,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
@@ -25,7 +27,26 @@ public class TabsAdapterTest extends FragmentActivity {
     }
 
     @Test
+    public void getItemReturnNullTest(){
+//        MedicineFragment medicineFragment = new MedicineFragment();
+//        UBSFragment ubsFragment = new UBSFragment();
+//
+//        assert
+//
+//        assertEquals(MedicineFragment.newInstance(), tabsAdapter.getItem(0));
+//        assertEquals(UBSFragment.newInstance(), tabsAdapter.getItem(1));
+        assertNull(tabsAdapter.getItem(2));
+    }
+
+    @Test
     public void getCountTest(){
         assertEquals(tabsAdapter.getCount(), 2);
+    }
+
+    @Test
+    public void getPageTitleTest(){
+        assertEquals("Remédio",tabsAdapter.getPageTitle(0));
+        assertEquals("UBS",tabsAdapter.getPageTitle(1));
+        assertNull(tabsAdapter.getPageTitle(2));
     }
 }
