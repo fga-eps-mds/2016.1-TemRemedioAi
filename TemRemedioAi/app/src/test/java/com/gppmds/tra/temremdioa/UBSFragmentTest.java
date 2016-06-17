4f101428bfd0203a75b2190bc581dc369437ec86
Gabriel Climaco <climaco.cafe@gmail.com>
2016-06-17 05:47:41 -0300

    Adding two tests

--- TemRemedioAi/app/src/test/java/com/gppmds/tra/temremdioa/UBSFragmentTest.java
+++ TemRemedioAi/app/src/test/java/com/gppmds/tra/temremdioa/UBSFragmentTest.java
@@ -2,7 +2,10 @@ package com.gppmds.tra.temremdioa;
 
 
 import android.os.Build;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.gppmds.tra.temremdioa.controller.fragment.MedicineFragment;
 import com.gppmds.tra.temremdioa.controller.fragment.UBSFragment;
@@ -15,6 +18,7 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 @RunWith(RobolectricGradleTestRunner.class)
@@ -26,12 +30,26 @@ public class UBSFragmentTest {
 
     @Before
     public void setup() throws Exception {
-        fragmentActivity = Robolectric.buildActivity(FragmentActivity.class).create().start().resume().get();
+        fragmentActivity = Robolectric.buildActivity(FragmentActivity.class).create().start()
+                            .resume().get();
         ubsFragment = new UBSFragment();
     }
 
     @Test
     public void newInstanceTest() {
         assertTrue(UBSFragment.class == ubsFragment.newInstance().getClass());
+    }
+
+    @Test
+    public void getUbsAdapterTest(){
+        addMapFragment(fragmentActivity, ubsFragment);
+        assertNotNull(ubsFragment.getUbsAdapter());
+    }
+
+    private void addMapFragment(FragmentActivity activity, Fragment fragment) {
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(fragment, "ubsFragment");
+        transaction.commit();
     }
 }
