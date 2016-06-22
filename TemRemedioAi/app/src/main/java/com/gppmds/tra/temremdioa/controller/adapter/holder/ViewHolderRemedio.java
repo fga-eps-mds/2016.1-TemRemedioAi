d8be02992084ee171d14ce8e77aee035aefe4f31
Guilherme <guilhermebaldissera@hotmail.com>
2016-06-22 15:36:58 -0300

    Include chart of medicine card list

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/adapter/holder/ViewHolderRemedio.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/adapter/holder/ViewHolderRemedio.java
@@ -14,11 +14,21 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.gppmds.tra.temremdioa.controller.Inform;
 import com.gppmds.tra.temremdioa.controller.SelectUBSActivity;
 import com.gppmds.tra.temremdioa.controller.adapter.CardListAdapterRemedio;
+import com.gppmds.tra.temremdioa.model.Notification;
 import com.gppmds.tra.temremdioa.model.Remedio;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.tra.gppmds.temremdioa.R;
+
+import java.util.ArrayList;
 
 public class ViewHolderRemedio extends RecyclerView.ViewHolder {
     public TextView textViewNomeMedicamento;
@@ -33,6 +43,8 @@ public class ViewHolderRemedio extends RecyclerView.ViewHolder {
     public ImageView imageViewArrow;
     public String ubsSelectedName;
 
+    private PieChart pieChart;
+
     public ViewHolderRemedio(CardView card) {
         super(card);
         this.textViewNomeMedicamento = (TextView) card.findViewById(R.id.textViewNomeMedicamento);
@@ -44,6 +56,7 @@ public class ViewHolderRemedio extends RecyclerView.ViewHolder {
         this.buttonMedicineInform = (Button) card.findViewById(R.id.buttonInformRemedio);
         this.expandLayout = (RelativeLayout) card.findViewById(R.id.expandable);
         this.headerLayout = (RelativeLayout) card.findViewById(R.id.header);
+        this.pieChart = (PieChart) card.findViewById(R.id.pie_chart_medicine);
 
         this.expandLayout.setVisibility(View.GONE);
 
@@ -70,6 +83,8 @@ public class ViewHolderRemedio extends RecyclerView.ViewHolder {
                 Log.i("LOG", "onClickListener of headerLayout clicked");
                 if (expandLayout.getVisibility() == View.GONE) {
                     Log.i("LOG", "Expand Click");
+                    Remedio selectItem = CardListAdapterRemedio.dataRemedio.get(ViewHolderRemedio.this.getAdapterPosition());
+                    setInformationOfChart(selectItem);
                     expand();
                 } else {
                     Log.i("LOG", "Collapse Click");
@@ -103,6 +118,62 @@ public class ViewHolderRemedio extends RecyclerView.ViewHolder {
                 view.getContext().startActivity(intent);
             }
         });
+    }
+
+    private void setInformationOfChart(Remedio remedio) {
+        pieChart.setDescription("");
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleRadius(7);
+        pieChart.setTransparentCircleRadius(10);
+        pieChart.setDrawSliceText(true);
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+        pieChart.setData(getDataPie(remedio));
+
+    }
+
+    public PieData getDataPie(Remedio remedio) {
+        PieData pieData = null;
+
+        Integer countNotificationAvailable = 0;
+        Integer countNotificationNotAvailable = 0;
+
+        ParseQuery<Notification> queryNotificationAvailable = Notification.getQuery();
+        queryNotificationAvailable.whereEqualTo(Notification.getTitleMedicineName(), remedio.getMedDes());
+        queryNotificationAvailable.whereEqualTo(Notification.getTitleMedicineDosage(), remedio.getMedDos());
+        queryNotificationAvailable.whereEqualTo(Notification.getTitleAvailable(), true);
+        try {
+            countNotificationAvailable = queryNotificationAvailable.count();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        ParseQuery<Notification> queryNotificationNotAvailable = Notification.getQuery();
+        queryNotificationNotAvailable.whereEqualTo(Notification.getTitleMedicineName(), remedio.getMedDes());
+        queryNotificationNotAvailable.whereEqualTo(Notification.getTitleMedicineDosage(), remedio.getMedDos());
+        queryNotificationNotAvailable.whereEqualTo(Notification.getTitleAvailable(), false);
+        try {
+            countNotificationNotAvailable = queryNotificationNotAvailable.count();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Entry> valuesAvailable = new ArrayList<Entry>();
+        ArrayList<String> valuesLegend = new ArrayList<String>();
+
+        valuesLegend.add("Sim");
+        valuesLegend.add("Não");
+
+        valuesAvailable.add(new Entry((float) countNotificationAvailable, 0));
+        valuesAvailable.add(new Entry((float) countNotificationNotAvailable, 1));
+
+        PieDataSet pieDataSet = new PieDataSet(valuesAvailable, "");
+        pieDataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        pieDataSet.setSliceSpace(2f);
+
+        pieData = new PieData(valuesLegend, pieDataSet);
+
+        return pieData;
     }
 
     private void expand() {
