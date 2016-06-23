df1700b4c2dbaf7e69d2db50efdb8604d46fc5a3
Guilherme <guilhermebaldissera@hotmail.com>
2016-06-23 05:03:05 -0300

    Configure local database of parse inicitializer

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/ParseInitializer.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/ParseInitializer.java
@@ -3,9 +3,13 @@ package com.gppmds.tra.temremdioa.controller;
 import com.gppmds.tra.temremdioa.model.Medicine;
 import com.gppmds.tra.temremdioa.model.Notification;
 import com.gppmds.tra.temremdioa.model.UBS;
+import com.parse.FindCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class ParseInitializer extends android.app.Application {
 
@@ -15,6 +19,10 @@ public class ParseInitializer extends android.app.Application {
 
         registerParserSubClasses();
         inicializeParser();
+
+        loadLocalDateBaseMedicine();
+        loadLocalDateBaseUBS();
+        loadLocalDateBaseNotification();
     }
 
     public boolean registerParserSubClasses() {
@@ -31,14 +39,13 @@ public class ParseInitializer extends android.app.Application {
     public boolean inicializeParser() {
         /* Establish connection with parse server */
         try {
-            Parse.enableLocalDatastore(this);
             Parse.initialize(new Parse.Configuration.Builder(this)
                     .applicationId("TemRemedioAi")
                     .server("http://temremedioai.herokuapp.com/temremedioai/Class")
                     .clientKey("kijasijijasiasjsiajalllkaosiajhsis")
+                    .enableLocalDataStore()
                     .build()
             );
-
 
             return true;
         } catch (Exception e) {
@@ -46,4 +53,52 @@ public class ParseInitializer extends android.app.Application {
         }
     }
 
+    public boolean loadLocalDateBaseMedicine() {
+        ParseQuery<Medicine> queryMedicine = Medicine.getQuery();
+        queryMedicine.setLimit(1000);
+        queryMedicine.findInBackground(new FindCallback<Medicine>() {
+            @Override
+            public void done(List<Medicine> list, ParseException e) {
+                if (e != null) {
+                    // Nothing to do
+                } else {
+                    Medicine.pinAllInBackground(list);
+                }
+            }
+        });
+        return true;
+    }
+
+    public boolean loadLocalDateBaseUBS() {
+        ParseQuery<UBS> queryUBS = UBS.getQuery();
+        queryUBS.setLimit(100);
+        queryUBS.findInBackground(new FindCallback<UBS>() {
+            @Override
+            public void done(List<UBS> list, ParseException e) {
+                if (e != null) {
+                    // Nothing to do
+                } else {
+                    UBS.pinAllInBackground(list);
+                }
+            }
+        });
+        return true;
+    }
+
+    public boolean loadLocalDateBaseNotification() {
+        ParseQuery<Notification> queryNotification = Notification.getQuery();
+        queryNotification.setLimit(1000);
+        queryNotification.findInBackground(new FindCallback<Notification>() {
+            @Override
+            public void done(List<Notification> list, ParseException e) {
+                if (e != null) {
+                    // Nothing to do
+                } else {
+                    Notification.pinAllInBackground(list);
+                }
+            }
+        });
+
+        return true;
+    }
 }
